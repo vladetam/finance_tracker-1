e65bf41f8bbc77d4ee309f646b4bf9c5b9fc5f79
Aleksa Antic <aleksa.antic@mera.com>
2026-02-23 20:32:21 +0100

    Delete Employee

--- src/main/java/orion/rs/demo/controller/EmployeeController.java
+++ src/main/java/orion/rs/demo/controller/EmployeeController.java
@@ -1,5 +1,37 @@
 package orion.rs.demo.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import orion.rs.demo.service.implementation.EmployeeServiceImplementation;
 
+@RestController
+@RequestMapping("api/employee")
+@RequiredArgsConstructor
 public class EmployeeController {
+
+    private final EmployeeServiceImplementation employeeService;
+
+
+    /***
+     * Deleting employee by ID if ID is valid , ID must be entered!
+     * API: http://localhost:8080/api/employee/deleteByID
+     */
+
+    @DeleteMapping(value = "/deleteByID")
+    public ResponseEntity<?> deleteEmployee(@RequestParam Long employee_id){
+        try {
+
+            employeeService.deleteEmployee(employee_id);
+            return ResponseEntity.status(HttpStatus.OK).body("Successfully deleted employee!");
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
 }
--- src/main/java/orion/rs/demo/service/implementation/EmployeeServiceImplementation.java
+++ src/main/java/orion/rs/demo/service/implementation/EmployeeServiceImplementation.java
@@ -1,4 +1,32 @@
 package orion.rs.demo.service.implementation;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import orion.rs.demo.domain.Employee;
+import orion.rs.demo.exceptionHandling.EmployeeNotFoundException;
+import orion.rs.demo.repository.EmployeeRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class EmployeeServiceImplementation {
+
+    private final EmployeeRepository employeeRepository;
+
+
+    /**
+     *
+    * Deleting employee by ID
+     *
+    * */
+
+    public void deleteEmployee(Long id_employee) throws Exception {
+
+        Employee employee = employeeRepository.findById(id_employee).orElseThrow(() ->
+                new EmployeeNotFoundException(id_employee));
+
+        employeeRepository.delete(employee);
+    }
+
 }
