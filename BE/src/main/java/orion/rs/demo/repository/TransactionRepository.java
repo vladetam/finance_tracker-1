945a66674ca8bb820e6122d4fb99e7d6afac8a57
nikolamecaninn <nikola.mecanin@orioninc.com>
2026-02-24 13:06:27 +0100

    Added TransactionSpecification

--- BE/src/main/java/orion/rs/demo/repository/TransactionRepository.java
+++ BE/src/main/java/orion/rs/demo/repository/TransactionRepository.java
@@ -1,6 +1,7 @@
 package orion.rs.demo.repository;
 import orion.rs.demo.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface TransactionRepository extends JpaRepository<Transaction, Long>{
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+public interface TransactionRepository extends JpaRepository<Transaction, Long>, JpaSpecificationExecutor<Transaction>{
 
 }
